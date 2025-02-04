49714a548fffd829b764a078b03a275002c76698
Burit S <burit3594@gmail.com>
2025-02-04 12:37:44 +0700

    config

--- src/vipmember/vip.module.ts
+++ src/vipmember/vip.module.ts
@@ -1,15 +1,13 @@
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Module } from "@nestjs/common";
-import { VipMember } from "./vip.entity";
-import { VipService } from "./vip.service";
-import { VipController } from "./vip.controller";
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { VipMember } from './vip.entity';
+import { VipService } from './vip.service';
+import { VipController } from './vip.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VipMember])],
-    providers: [VipService],
-    controllers: [VipController],
-    exports: [VipService],
-  })
+  imports: [TypeOrmModule.forFeature([VipMember])],
+  providers: [VipService],
+  controllers: [VipController],
+  exports: [VipService],
+})
 export class VipModule {}
-
